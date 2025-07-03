e290caa6b90e6487c0b5ada09c23281252787258
Vivek_Mohape <139157553+VivekMohape@users.noreply.github.com>
2025-07-03 16:18:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ from lxml import html
 from instructor import from_openai
 from pydantic import BaseModel, Field
 import openai
-from langsmith.trace import traceable
+from langsmith.wrappers import traceable
+
 
 
 # Structured schema for output
